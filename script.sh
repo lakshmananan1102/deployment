11049f3f525a29e04bb2450280ed2cdd92652f9c
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-05-06 01:35:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 AUTH=`echo -n aniccadeveloper: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 | base64`
 echo $AUTH
-echo '{"auths":{"ghcr.io":{"auth":$AUTH}}}' | kubectl create secret generic dockerconfigjson-github-com --from-file=.dockerconfigjson=/dev/stdin
+echo '{"auths":{"ghcr.io":{"auth":$AUTH}}}' | sudo microk8s kubectl create secret generic dockerconfigjson-github-com --from-file=.dockerconfigjson=/dev/stdin
