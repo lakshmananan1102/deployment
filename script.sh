e330833d4ae2a3485a25bc54d72e0d8e00be1a0c
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-06-20 12:37:41 +0530

    change script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,35 @@
-AUTH=`echo -n aniccadeveloper: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 | base64`
-echo $AUTH
-echo '{"auths":{"ghcr.io":{"auth":$AUTH}}}' | sudo microk8s kubectl create secret generic dockerconfigjson-github-com --from-file=.dockerconfigjson=/dev/stdin
+export AKDC_CLUSTER=ch-bradenton-6489
+export AKDC_REPO=aniccadeveloper/Gitops-pipeline
+export AKDC_PAT=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
+export AKDC_BRANCH=voe-app
+
+flux bootstrap git \
+--url "https://github.com/$AKDC_REPO" \
+--branch main \
+--password "$AKDC_PAT" \
+--token-auth true \
+--path "./deploy/bootstrap/$AKDC_CLUSTER"
+
+flux create secret git gitops \
+--url "https://github.com/$AKDC_REPO" \
+--password "$AKDC_PAT" \
+--username gitops
+
+flux create source git gitops \
+--url "https://github.com/$AKDC_REPO" \
+--branch "$AKDC_BRANCH" \
+--secret-ref gitops
+
+flux create kustomization bootstrap \
+--source GitRepository/gitops \
+--path "./deploy/bootstrap/$AKDC_CLUSTER" \
+--prune true \
+--interval 1m
+
+flux create kustomization apps \
+--source GitRepository/gitops \
+--path "./deploy/apps/$AKDC_CLUSTER" \
+--prune true \
+--interval 1m
+
+flux reconcile source git gitops
