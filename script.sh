191bef530c987bfd14592b574bf885a7ff8b46d9
sidharth <87812643+sidharthram98@users.noreply.github.com>
2022-07-04 21:08:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,33 +1,29 @@
-export AKDC_CLUSTER=store_dir
-export AKDC_REPO=aniccadeveloper/Cloud-Gitos
-export AKDC_PAT=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
-export AKDC_BRANCH=voe-app
 
 flux bootstrap git \
---url "https://github.com/$AKDC_REPO" \
---branch "$AKDC_BRANCH" \
---password "$AKDC_PAT" \
+--url "https://github.com/$GITOPS_REPO" \
+--branch "$GITOPS_BRANCH" \
+--password "$GITOPS_PAT" \
 --token-auth true \
---path "./deploy/bootstrap/$AKDC_CLUSTER"
+--path "./deploy/bootstrap/$GITOPS_CLUSTER"
 
 flux create secret git gitops \
---url "https://github.com/$AKDC_REPO" \
---password "$AKDC_PAT" \
+--url "https://github.com/$GITOPS_REPO" \
+--password "$GITOPS_PAT" \
 --username gitops
 
 flux create source git gitops \
---url "https://github.com/$AKDC_REPO" \
---branch "$AKDC_BRANCH" \
+--url "https://github.com/$GITOPS_REPO" \
+--branch "$GITOPS_BRANCH" \
 --secret-ref gitops
 
 flux create kustomization bootstrap \
 --source GitRepository/gitops \
---path "./deploy/bootstrap/$AKDC_CLUSTER" \
+--path "./deploy/bootstrap/$GITOPS_CLUSTER" \
 --prune true \
 --interval 1m
 
 flux create kustomization apps \
 --source GitRepository/gitops \
---path "./deploy/apps/$AKDC_CLUSTER" \
+--path "./deploy/apps/$GITOPS_CLUSTER" \
 --prune true \
 --interval 1m
